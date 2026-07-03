fb59acedae25ff50803036a0308581306e5dfabf
sanctificare <294658636+sanctificare@users.noreply.github.com>
2026-07-03 16:36:40 -0300

    fix: substitui icone de maos em oracao por design mais limpo e visivel

--- client/src/components/PrayingHandsIcon.tsx
+++ client/src/components/PrayingHandsIcon.tsx
@@ -12,28 +12,23 @@ export function PrayingHandsIcon({
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="1.5"
+      strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
       className={className}
       {...props}
     >
-      {/* Center dividing line where index fingers/palms touch */}
+      {/* Symmetrical Praying Hands (Pressed Palms) */}
+      <path d="M12 3c0 0-3.5 4-3.5 9c0 4.5 2.5 6.5 3.5 8.5" />
+      <path d="M12 3c0 0 3.5 4 3.5 9c0 4.5-2.5 6.5-3.5 8.5" />
+      
+      {/* Contact Line */}
       <line x1="12" y1="3" x2="12" y2="17" />
-
-      {/* Diverging wrists at the bottom */}
-      <path d="M 12,17 C 11,19.5 10,21.5 9,23" />
-      <path d="M 12,17 C 13,19.5 14,21.5 15,23" />
-
-      {/* Left Hand Contours */}
-      <path d="M 3,20 C 4.5,18 7,14 7,10 C 7,6 9.5,3 12,3" />
-      <path d="M 5,21.5 C 6.2,19.5 8.5,16 8.5,12 C 8.5,8.5 10.2,6 12,6" />
-      <path d="M 7.5,22.5 C 8.5,20.8 10,17.5 10,14 C 10,11.2 11,9 12,9" />
-
-      {/* Right Hand Contours */}
-      <path d="M 21,20 C 19.5,18 17,14 17,10 C 17,6 14.5,3 12,3" />
-      <path d="M 19,21.5 C 17.8,19.5 15.5,16 15.5,12 C 15.5,8.5 13.8,6 12,6" />
-      <path d="M 16.5,22.5 C 15.5,20.8 14,17.5 14,14 C 14,11.2 13,9 12,9" />
+      
+      {/* Sleeves / Cuff details */}
+      <path d="M8 21h8" />
+      <path d="M9.5 21v2" />
+      <path d="M14.5 21v2" />
     </svg>
   );
 }
